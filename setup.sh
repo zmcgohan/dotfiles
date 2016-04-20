07d04b61cff6bf9b908c82c05975362b0536da00
Zachary McGohan <zmcgohan@gmail.com>
2016-04-19 21:46:15 -0400

    Added lines from virtual machine to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,50 @@
-git config --global user.name "Zach McGohan"
-git config --global user.email "zmcgohan@gmail.com"
-git config --global core.editor vim
+: '
+This is my personal config file for all Linux operating systems. It is
+written so that you (or, more likely me, I guess) can simply run the 
+script on a new installation and have a fully configured system. 
 
-# create symlink for info bar wrapper (allows full customization of text)
-ln -s ~/system-config/i3status_wrapper.py ~/.i3/i3status_wrapper.py
+Git should be installed and this repo cloned, but other than that there
+should not be any further manual work unless noted.
+'
 
+# UBUNTU
+# sudo apt-get install vim python i3 chromium-browser
+# sudo apt-get install nodejs npm
+# sudo apt-get install libopencv-dev python-opencv
+
+# Bashrc setup
 # make sure bashrc exists
 touch ~/.bashrc
 # add custom bashrc commands to bottom of ~/.bashrc
 cat .bashrc_custom >> ~/.bashrc
 
-# copy custom programs to /usr/local/bin/
+# Git setup
+git config --global user.name "Zach McGohan"
+git config --global user.email "zmcgohan@gmail.com"
+git config --global core.editor vim
+
+# i3 setup
+# Install
+echo 'deb http://dl.bintray.com/i3/i3-autobuild-ubuntu wily main' > /etc/apt/sources.list.d/i3-autobuild.list
+apt-get update
+apt-get --allow-unauthenticated install i3-autobuild-keyring
+apt-get update
+apt-get install i3
+# Create symbolic link to config file
+ln -s ~/system-configs/i3-config ~/.i3/config
+# create symlink for info bar wrapper (allows full customization of text)
+ln -s ~/system-config/i3status_wrapper.py ~/.i3/i3status_wrapper.py
+
+# Vim setup
+# Clone Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install plugins
+vim +PluginInstall +qall
+# Create symbolic link to config file
+ln -s ~/system-configs/.vimrc ~/.vimrc
+
+# Create projects folder
+mkdir ~/projects
+
+# create symlinks for custom programs in /usr/local/bin/
 ln -s /usr/local/bin/* bin/
