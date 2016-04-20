676da1a9c76a93d209ce0631b2efa9765cda2ba0
Zach McGohan <zmcgohan@gmail.com>
2016-04-19 21:31:15 -0400

    Added custom programs and bashrc commands.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 git config --global user.name "Zach McGohan"
 git config --global user.email "zmcgohan@gmail.com"
 git config --global core.editor vim
+
+# create symlink for info bar wrapper (allows full customization of text)
+ln -s ~/system-config/i3status_wrapper.py ~/.i3/i3status_wrapper.py
+
+# make sure bashrc exists
+touch ~/.bashrc
+# add custom bashrc commands to bottom of ~/.bashrc
+cat .bashrc_custom >> ~/.bashrc
+
+# copy custom programs to /usr/local/bin/
+ln -s /usr/local/bin/* bin/
