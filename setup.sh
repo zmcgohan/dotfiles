dc525381023ffdf95bb16862773befc4095bf683
Zach McGohan <zmcgohan@gmail.com>
2016-04-20 00:08:24 -0400

    Fixed vim setup and reorganized a lot of code.
    Added OpenCV and Python installations.

--- setup.sh
+++ setup.sh
@@ -7,10 +7,19 @@ Git should be installed and this repo cloned, but other than that there
 should not be any further manual work unless noted.
 '
 
-# UBUNTU
-sudo apt-get -y install vim python i3 chromium-browser
+### UBUNTU SPECIFIC ###
+sudo apt-get -y install vim python python-pip python-dev i3 chromium-browser
+# Node.js
 sudo apt-get -y install nodejs npm
+# OpenCV
 sudo apt-get -y install libopencv-dev python-opencv
+pip install matplotlib pyscreenshot
+# i3
+echo 'deb http://dl.bintray.com/i3/i3-autobuild-ubuntu wily main' > /etc/apt/sources.list.d/i3-autobuild.list
+sudo apt-get update
+sudo apt-get -y --allow-unauthenticated install i3-autobuild-keyring
+sudo apt-get update
+sudo apt-get -y install i3
 
 # Bashrc setup
 # make sure bashrc exists
@@ -24,12 +33,6 @@ git config --global user.email "zmcgohan@gmail.com"
 git config --global core.editor vim
 
 # i3 setup
-# Install
-echo 'deb http://dl.bintray.com/i3/i3-autobuild-ubuntu wily main' > /etc/apt/sources.list.d/i3-autobuild.list
-sudo apt-get update
-sudo apt-get -y --allow-unauthenticated install i3-autobuild-keyring
-sudo apt-get update
-sudo apt-get -y install i3
 # Create i3 folder in home directory
 mkdir -p ~/.i3
 # Create symbolic link to config file
@@ -40,10 +43,10 @@ ln -s $(pwd)/i3wm/i3status_wrapper.py ~/.i3/i3status_wrapper.py
 # Vim setup
 # Clone Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Install plugins
-vim +PluginInstall +qall
 # Create symbolic link to config file
 ln -s $(pwd)/.vimrc ~/.vimrc
+# Install plugins
+vim +PluginInstall +qall
 
 # Create SSH folder if it doesn't exist
 mkdir -p ~/.ssh
@@ -61,6 +64,8 @@ ssh-add
 mkdir -p ~/projects/testing
 mkdir ~/projects/resources
 
-# create symlinks for custom programs in /usr/local/bin/
+# Create symlinks for custom programs in /usr/local/bin/
 sudo cp -rs $(pwd)/bin/* /usr/local/bin/
 
+# Reload bashrc
+source ~/.bashrc
